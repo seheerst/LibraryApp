d15ad6ab72ab57b24390cb7809e76d0b8c2191a3
seheerst <seherst9@gmail.com>
2023-08-29 18:44:53 +0300

    feat: created dbContext

---
+++ LibraryApp/Utility/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApp.Utility
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<BookType> BookType { get; set;}
+    }
+}
