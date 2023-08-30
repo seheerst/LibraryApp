09c385cb6eb696717b32729d201fc9d498e13172
seheerst <seherst9@gmail.com>
2023-08-30 14:02:13 +0300

    feat: created controller

---
+++ LibraryApp/Controllers/BookTypeController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryApp.Models;
+using LibraryApp.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+    public class BookTypeController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public BookTypeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<BookType> bookTypes = _context.BookType.ToList();
+            return View(bookTypes);
+        }
+    }
+}
