e6421f7b8aabbb1f81faa977152c83f5bd2d1cf4
seheerst <seherst9@gmail.com>
2023-09-02 21:02:24 +0300

    feat: added repository

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IBookTypeRepository, BookTypeRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
 
 var app = builder.Build();
 
