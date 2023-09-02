837813114b290fc9202c3a50213d789a42ad2e1a
seheerst <seherst9@gmail.com>
2023-09-02 20:55:44 +0300

    feat: created Book model

---
+++ LibraryApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryApp.Models
+{
+    public class Book
+    {
+
+        public int BookId { get; set; }
+
+        public string BookName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public Double Price { get; set; }
+
+
+    }
+}
