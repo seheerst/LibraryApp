7c9d9887b90aea739a143b425854e32d418d55e9
seheerst <seherst9@gmail.com>
2023-09-03 15:46:38 +0300

    feat: added foreign key

---
+++ LibraryApp/Migrations/20230903104207_ForeignKeyAdd.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForeignKeyAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TypeId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_TypeId",
+                table: "Books",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookType_TypeId",
+                table: "Books",
+                column: "TypeId",
+                principalTable: "BookType",
+                principalColumn: "TypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookType_TypeId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_TypeId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                table: "Books");
+        }
+    }
+}
