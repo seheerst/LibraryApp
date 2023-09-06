5d146010c64167c5a71f580ace04d16f9d0c324b
seheerst <seherst9@gmail.com>
2023-09-06 17:45:36 +0300

    feat: created model

---
+++ LibraryApp/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [Required]
+        public int UserNo { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Fakulte { get; set; }
+
+        public string? Bolum { get; set; }
+
+    }
+}
